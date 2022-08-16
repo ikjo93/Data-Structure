67e12b79681921332ee47c8d0c84b6317e763be3
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-08-16 23:41:05 +0900

    Update BinaryTreeTest.java

--- 이진트리/BinaryTreeTest.java
+++ 이진트리/BinaryTreeTest.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import tree.BinaryTree.Node;
 
 public class BinaryTreeTest {
 
@@ -20,23 +21,32 @@ public class BinaryTreeTest {
 
         System.out.println(binaryTree.size()); // 7
 
+        Node rootNode = binaryTree.getRootNode();
+
+        System.out.println();
+        System.out.println("====너비 우선 탐색=====");
+        List<Integer> values = binaryTree.bfs(rootNode);
+        for (Integer value : values) {
+            System.out.printf("%d ", value); // 5 3 7 1 4 6 8
+        }
+
         System.out.println();
         System.out.println("====전위 순회=====");
-        List<Integer> values = binaryTree.preOrder();
+        values = binaryTree.preOrder(rootNode);
         for (Integer value : values) {
             System.out.printf("%d ", value); // 5 3 1 4 7 6 8
         }
 
         System.out.println();
         System.out.println("====중위 순회=====");
-        values = binaryTree.inOrder();
+        values = binaryTree.inOrder(rootNode);
         for (Integer value : values) {
             System.out.printf("%d ", value); // 1 3 4 5 6 7 8
         }
 
         System.out.println();
         System.out.println("====후위 순회=====");
-        values = binaryTree.postOrder();
+        values = binaryTree.postOrder(rootNode);
         for (Integer value : values) {
             System.out.printf("%d ", value); // 1 4 3 6 8 7 5
         }
