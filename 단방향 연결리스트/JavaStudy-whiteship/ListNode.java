77f1c4bfc136449f58174b9bf2eaccf9eca0a7a6
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-08-18 21:41:23 +0900

    Create ListNode.java

---
+++ 단방향 연결리스트/JavaStudy-whiteship/ListNode.java
@@ -0,0 +1,47 @@
+package whiteship;
+
+public class ListNode {
+
+    private final int value;
+    private ListNode nextNode;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    public void add(ListNode listNode) {
+        ListNode node = this;
+
+        while (node.nextNode != null) {
+            node = node.nextNode;
+        }
+
+        node.nextNode = listNode;
+    }
+
+    public int size() {
+        int size = 1;
+
+        ListNode node = this;
+
+        while (node.nextNode != null) {
+            size++;
+            node = node.nextNode;
+        }
+
+        return size;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public ListNode getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(ListNode listNode) {
+        this.nextNode = listNode;
+    }
+
+}
