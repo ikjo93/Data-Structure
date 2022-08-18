d25d57d7aead861b1af424ad84fbae3ae660eeb6
ikjo93 <82401504+ikjo93@users.noreply.github.com>
2022-08-19 02:24:22 +0900

    Create Stack.java

---
+++ JavaStudy-whiteship/스택/Stack.java
@@ -0,0 +1,64 @@
+package whiteship.stack;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int[] values;
+    private int size = 0;
+
+    public Stack() {
+        this(DEFAULT_CAPACITY);
+    }
+    
+    public Stack(int capacity) {
+        values = new int[capacity];
+    }
+
+    public void push(int data) {
+        values[size++] = data;
+        if (size == values.length) {
+            sizeUp();
+        }
+    }
+
+    public int pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+
+        int result = values[--size];
+        values[size] = 0;
+
+        if (size < (values.length / 2)) {
+            sizeDown();
+        }
+
+        return result;
+    }
+
+    private void sizeUp() {
+        int newCapacity = values.length * 2;
+        values = Arrays.copyOf(values, newCapacity);
+    }
+
+    private void sizeDown() {
+        int newCapacity = (values.length / 2);
+        values = Arrays.copyOf(values, Math.max(DEFAULT_CAPACITY, newCapacity));
+    }
+
+    public int get(int index) {
+        return values[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        values = new int[DEFAULT_CAPACITY];
+        size = 0;
+    }
+}
